0c56bbc6363820a44f32eff88b7364ff71202229
João Bispo <joaobispo2077@gmail.com>
2022-01-15 18:42:36 -0300

    feat: segregate moduleAliases environment

--- src/config/moduleAliases.ts
+++ src/config/moduleAliases.ts
@@ -2,9 +2,20 @@ import moduleAlias from 'module-alias';
 
 import * as path from 'path';
 
+const { NODE_ENV } = process.env;
+
 const rootDirectory = path.resolve(__dirname, '../../');
 
+const projectFilesDirectoryByEnvironments: Record<string, string> = {
+  development: 'src',
+  production: 'dist',
+};
+
+const projectFilesDirectory =
+  projectFilesDirectoryByEnvironments[String(NODE_ENV)] ||
+  projectFilesDirectoryByEnvironments.development;
+
 moduleAlias.addAliases({
-  '@src': path.join(rootDirectory, 'dist'),
+  '@src': path.join(rootDirectory, projectFilesDirectory),
   '@tests': path.join(rootDirectory, 'tests'),
 });
